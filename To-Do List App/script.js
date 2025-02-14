db070f2880bae46b2523a26ac50fc09418f584a5
Biagiarchiefz <biagiiarchie@gmail.com>
2025-02-15 03:06:08 +0700

    finis

--- To-Do List App/script.js
+++ To-Do List App/script.js
@@ -10,9 +10,40 @@ function addTask(){
     alert("masukan tugas anda")
   } else {
     let li = document.createElement("li");
-    li.innerHTML = input.value;
-    listContainer.appendChild(li)
+    li.innerHTML = input.value;  /* .value mengambil nilai inputan */
+    listContainer.appendChild(li); /* appendChild(li) adalah sebuah metode yang digunakan untuk menambahkan elemen HTML sebagai anak (child) ke dalam elemen lain. */
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"
+    li.appendChild(span)
   }
 
+  input.value = "";
+  saveData();
+
 }
+
+
+listContainer.addEventListener("click", function(e){ /* ketika di click jalankan function */
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("check");
+    saveData();
+  } else if (e.target.tagName === "SPAN"){
+    e.target.parentElement.remove("")
+    saveData();
+  }
+
+
+})
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function simpan(){
+  listContainer.innerHTML = localStorage.getItem("data")
+}
+
+simpan();
+
 
