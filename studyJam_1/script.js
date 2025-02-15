88747b4c49d0e3031231ae36a414c92fa7f9a454
Biagiarchiefz <biagiiarchie@gmail.com>
2025-02-15 10:58:50 +0700

    push

---
+++ studyJam_1/script.js
@@ -0,0 +1,21 @@
+const kotak = document.getElementById("kotak");
+const btn = document.getElementById("btn")
+
+
+function blue () {
+  kotak.style.backgroundColor = "blue";
+}
+
+function red () {
+  kotak.style.backgroundColor = "red";
+}
+
+function green () {
+  kotak.style.backgroundColor = "green";
+}
+
+
+// btn.addEventListener("click", function(){
+//   kotak.style.backgroundColor = "black"
+// })
+
